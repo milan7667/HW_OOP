1ec296a8c4b4477d134c7de158f88ebed39c6eda
milan7667 <34488823+milan7667@users.noreply.github.com>
2019-12-28 13:43:59 +0200

    Update Elements.java
    Fixing class structure

--- Homework6/src/hw6/Elements.java
+++ Homework6/src/hw6/Elements.java
@@ -5,22 +5,24 @@ import java.util.List;
 
 public class Elements {
     public static void main(String[] args) {
-            List<Integer> myFirstList = new ArrayList<>();
-            List<Integer> mySecondList = new ArrayList<>();
-
-            myFirstList.add(1);
-            myFirstList.add(2);
-            myFirstList.add(3);
-
-            mySecondList.add(3);
-            mySecondList.add(4);
-            mySecondList.add(5);
-
-            myFirstList.retainAll(mySecondList);
-
-        System.out.println(myFirstList);
+        Elements.getElement();
     }
 
+    public static void getElement() {
+        List<Integer> myFirstList = new ArrayList<>();
+        List<Integer> mySecondList = new ArrayList<>();
+
+        myFirstList.add(1);
+        myFirstList.add(2);
+        myFirstList.add(3);
+
+        mySecondList.add(3);
+        mySecondList.add(4);
+        mySecondList.add(5);
+
+        myFirstList.retainAll(mySecondList);
+        System.out.println(myFirstList);
+    }
 }
 
 
